fe6da77f67c21d22daae565dc6b61c895f4e040c
Lunar <emmanielsen428@gmail.com>
2026-05-29 21:14:22 +0200

    Update ItsLunarHubBase.lua

--- ItsLunarHubBase.lua
+++ ItsLunarHubBase.lua
@@ -37,39 +37,7 @@ end
 
 if game.PlaceId == 13822889 then
     -- Lumber Tycoon 2
-
-    local scripts = {
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar1.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar2.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar3.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar4.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar5.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar6.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar7.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar8.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar9.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar10.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar11.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar12.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar13.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar14.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar15.lua",
-        "https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/Lunar16.lua",
-    }
-
-    for _, url in ipairs(scripts) do
-        local content = fetch(url)
-
-        if content then
-            local func = loadstring(content)
-
-            if func then
-                task.spawn(func)
-            end
-        end
-
-        task.wait(0.2)
-    end
+    loadstring(fetch("https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/ItsLunarHub.lua"))()
 else
     LP:Kick("LunarHub: This game is not supported.")
 end
