3e9b0caf1ec252cfcd14a9d6d5798e1551904bf2
Lunar <emmanielsen428@gmail.com>
2026-05-29 21:03:58 +0200

    Create ItsLunarHubBase.lua

---
+++ ItsLunarHubBase.lua
@@ -0,0 +1,42 @@
+local KEY = getgenv().ItsLunarHubBase
+local LP = game:GetService("Players").LocalPlayer
+
+local function fetch(url)
+    local ok, res = pcall(function()
+        return game:HttpGet(url)
+    end)
+    return ok and res or nil
+end
+
+if not KEY or KEY == "" then
+    LP:Kick("LunarHub: Please provide a key in the script!")
+    return
+end
+
+local keyData = fetch("https://raw.githubusercontent.com/ItsLunarHub/ItsLunarHubBase/main/keys.txt")
+if not keyData then
+    LP:Kick("LunarHub: Could not reach key server!")
+    return
+end
+
+local keyValid = false
+for line in keyData:gmatch("[^\r\n]+") do
+    if line:match("^%s*(.-)%s*$") == KEY then
+        keyValid = true
+        break
+    end
+end
+
+if not keyValid then
+    LP:Kick("LunarHub: Invalid or Expired Key")
+    return
+end
+
+if game.PlaceId == 13822889 then
+    -- Lumber Tycoon 2
+    loadstring(fetch("https://raw.githubusercontent.com/ItsLunarHub/LunarHubLT2/main/LunarHubLT2.lua"))()
+else
+    LP:Kick("LunarHub: This game is not supported.")
+end
+
+getgenv().ItsLunarHubBase = nil
